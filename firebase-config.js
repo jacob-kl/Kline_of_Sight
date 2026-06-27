51f321785c376d78fb2e6a7ba387b8242b1ea2e7
jacob-kl <49077208+jacob-kl@users.noreply.github.com>
2026-06-27 11:06:51 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBhmHbskJtHo9FVXr0QoC0nM3iijBIipow",
+  authDomain: "kline-of-sight.firebaseapp.com",
+  projectId: "kline-of-sight",
+  storageBucket: "kline-of-sight.firebasestorage.app",
+  messagingSenderId: "371974061736",
+  appId: "1:371974061736:web:f0719f62973f01b1b20c9d",
+  measurementId: "G-PWTJNQ5Q1W"
+};
